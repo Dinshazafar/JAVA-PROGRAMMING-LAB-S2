8cf39e1fccb4fef270bcc3a9037cdfa9654603cc
DINSHA ZAFAR <95676736+Dinshazafar@users.noreply.github.com>
2022-04-06 11:26:26 +0530

    Add files via upload

---
+++ Circle/Circle.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Circle
+{
+ int r;
+double area;
+Circle(int rad)
+{
+ r=rad;
+}
+void calcArea()
+{
+ area=3.14*r*r;
+ System.out.println("Area is:"+area);
+}
+public static void main(String[] args)
+ {
+  Scanner in=new Scanner(System.in);
+  System.out.println("Enetr the radius:");
+  int r=in.nextInt();
+  Circle c=new Circle(r);
+  c.calcArea();
+ }
+}
+
